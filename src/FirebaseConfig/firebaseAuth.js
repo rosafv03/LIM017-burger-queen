1b331f634594294d88c1f32d405482a080b603e1
gabriela tornel <tornelgabriela@gmail.com>
2022-05-31 16:39:33 -0500

    configuracion de inicialización de firebase

---
+++ src/FirebaseConfig/firebaseAuth.js
@@ -0,0 +1,11 @@
+import {getAuth,signOut, createUserWithEmailAndPassword , signInWithEmailAndPassword,} from 'firebase/auth';
+import {app} from "init.js"
+
+export const auth = getAuth(app);
+
+export const registerWithEmail = (email, password) => (
+    createUserWithEmailAndPassword(auth, email, password));
+
+export const loginUser = (email, password) => signInWithEmailAndPassword(auth, email, password);
+    
+export const signOff = () => signOut(auth);
---
+++ src/RouterComponents/Home.js
@@ -0,0 +1,1 @@
+import {signOff } from '../FirebaseConfig/firebaseAuth.js';
