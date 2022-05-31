78620f2cb842dd6c180e09d7d2c54afc3d974adf
gabriela tornel <tornelgabriela@gmail.com>
2022-05-31 10:46:08 -0500

    instalacion firebase -g

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Aqui va el proyecto burger queen rosita y gaby♥
         </p>
         <a
           className="App-link"
