c804b7c2ca99c1f0ffade41809360296897472bd
gabriela tornel <tornelgabriela@gmail.com>
2022-06-07 17:55:29 -0500

    creado funcion para filtrara pr categoria

--- src/RouterComponents/Home.jsx
+++ src/RouterComponents/Home.jsx
@@ -1,24 +1,13 @@
 
-import React, { useEffect ,useState} from 'react'
+import React, { } from 'react'
 import './Home.css';
 import banner from '../assets/img/banner.png';
-import { getData } from '../FirebaseConfig/FirestoreDB';
-// import { useFirestore } from '../hooks/FirestoreConfig.js';
+import { Lunch } from './Lunch';
+import { BreackFast } from './BreackFast';
 
 export const Home = () => {
 
-  const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-
-      const itemsDB = await getData();
-      console.log('trayendo items', itemsDB)
-      setItems(itemsDB)
-    }
-    fetchData();
-
-  }, []);
+ 
 
   // const [data, setData] = useState();
 
@@ -31,9 +20,11 @@ export const Home = () => {
   //   }
 
   //  } 
+  // const buttonCCC = () => {
 
-  const butonAgregando = () => {
-    console.log('agregando al pedido')
+  // }
+  const trayendoing = () =>{
+    console.log('hola almuerzo')
   }
   // if(loading) return <h1>Cargando Menu Data.....</h1>
   return (
@@ -45,32 +36,22 @@ export const Home = () => {
       <h1>Tomar pedido</h1>
 
       <div>
-        <label for='name'>Nombre cliente</label>
+        <label >Nombre cliente</label>
         <input type='text' name='name' />
       </div>
       <div>
-        <h2>MENU</h2>
+        <h2> MENU </h2>
         <button type='button'>Desayuno</button>
-        <button type='button'>Almuerzo</button>
+        <button type='button' onClick={trayendoing}>Almuerzo</button>
       </div>
       <div className="breackFast">
+      <BreackFast />
       </div>
       <div className='lunch'>
+       <Lunch/>
       </div>
       <div className='conteinerOrder'>
-        <>
-          {
-            items.map((item) => (
-            <div>
-          <button key={item.id} onClick={butonAgregando}>
-            {item.items}
-            {item.price}$
-            {item.uid}
-          </button>
-          </div>
-          ))
-        }
-        </>
+      
         {/* ------------obtener al hacer click el items ----------*/}
         {/* ------------tener en cuenta los items para sumarlos y no repetirlos ----------*/}
         {/* ------------mostrar el contenido de la orden----------*/}
---
+++ src/RouterComponents/Lunch.jsx
@@ -0,0 +1,40 @@
+
+import React, { useEffect ,useState} from 'react'
+import './Home.css';
+import { itemSortLuch } from '../FirebaseConfig/FirestoreDB';
+// import { useFirestore } from '../hooks/FirestoreConfig.js';
+
+
+export const Lunch = () => {
+    const [itemsLunch, setItemsLunch] = useState([]);
+    
+  useEffect(() => {
+    const fetchData = async () => {
+      const itemsDB = await itemSortLuch();
+      console.log('trayendo items', itemsDB)
+      setItemsLunch(itemsDB)
+    }
+    fetchData();
+  }, []);
+  
+  const butonAgregando = () => {
+    console.log('agregando al pedido')
+  }
+  return (
+   <div>Lunch
+        <>
+          {
+            itemsLunch.map((item) => (
+           <div>
+        <button key={item.id} onClick={butonAgregando}>
+           {item.items}
+           {item.price} $
+            {item.uid}
+        </button>
+          </div>
+          ))
+        }
+        </>
+   </div>
+  )
+}
