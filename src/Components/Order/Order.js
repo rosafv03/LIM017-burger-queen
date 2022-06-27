27d15d3a81bd1b85667459e15de715ffae6c9dbb
gabriela tornel <tornelgabriela@gmail.com>
2022-06-27 10:07:49 -0500

    modificacion componente Kitchen

---
+++ src/Components/Order/Order.js
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+
+
+// export const Order = ({setCart, cart, itemsbreakfast, item}) => {
+//   const {items, price } = itemsbreakfast
+//   return (
+//    <div>
+//    <ul>
+//     <li>{items}</li>
+//     <li>{price}</li>
+//     <button > agregar</button>
+//    </ul>
+
+//    </div>
+//   )
+// }
